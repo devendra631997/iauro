6a8b3b1103ac8b7f5812c0201f4f776fd30ebeb8
devendra <you@example.com>
2022-06-08 22:43:15 +0530

    update

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
+mongoose
+  .connect(
+    "mongodb://mongodb-myapp:27017/myapp",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .then(() =>{
+    SeedDB()
+  })
+  .catch(err => console.log(err));
 const server = require('./server/app');
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Localhost:${PORT}`));
