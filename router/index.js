72e93f007dbfbd93629095ba715a6d017644f9ef
devendra <you@example.com>
2022-06-11 01:10:01 +0530

    removed console

--- router/index.js
+++ router/index.js
@@ -15,7 +15,6 @@ const jwt = require('jsonwebtoken');
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    console.log(token,"token")
     if (token == null) return res.sendStatus(401)
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403)
